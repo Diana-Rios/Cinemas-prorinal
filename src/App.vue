28730e534edb2e587f1a37ca5f53103b755b7280
rina plata <rinamarcelaplata@gmail.com>
2023-11-01 02:17:05 -0500

    prorinal

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="badass-todo">
+
+  <div class="title has-text-centered">
+ tareas 
+  </div>
+
+
+  <div class="field is-grouped">
+  <p class="control is-expanded">
+    <input class="input" type="text" placeholder="add a todo">
+  </p>
+  <p class="control">
+    <a class="button is-info">
+      Add
+    </a>
+  </p>
+</div>
+</div>
+
+
+</template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+.badass-todo{
+  position: relative;
+  top: 20px;
+  max-width: 500px;
+  padding-left: 20px;
+  margin: 0 auto;
+}
+</style>
